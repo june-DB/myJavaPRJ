0afa7c952b3aa9caf284ab16795f2c9f26a4c2e6
도병준 <ihate3lue@gmail.com>
2025-03-19 16:39:33 +0900

    ad(h)d hook

---
+++ src/main/java/Baekjoon1000.java
@@ -0,0 +1,9 @@
+public class Baekjoon1000 {
+    public static void main(String[] args) {
+        int A = 100;
+        int B = 200;
+
+        int C = A + B;
+        System.out.println(C);
+    }
+}
