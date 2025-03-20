b6616f0e220e5b03dcc0cb0b34043ef1268e8007
도병준 <ihate3lue@gmail.com>
2025-03-20 19:14:45 +0900

    ADD issues

---
+++ src/main/java/Baekjoon1809.java
@@ -0,0 +1,11 @@
+public class Baekjoon1809 {
+    public static void main(String[] args) {
+        System.out.println("(   )");
+        System.out.println(" ---");
+        System.out.println("(o o)----/");
+        System.out.println(" @@      l ");
+        System.out.println("  l ----,/");
+        System.out.println("  //   //");
+        System.out.println(" ^^   ^^");
+    }
+}
