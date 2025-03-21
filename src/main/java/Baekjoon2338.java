5b13f82366074128f32b0424b7dfebaca57d04b1
도병준 <ihate3lue@gmail.com>
2025-03-21 17:09:13 +0900

    ADD issues

--- src/main/java/Baekjoon2338.java
+++ src/main/java/Baekjoon2338.java
@@ -3,13 +3,16 @@ import java.util.Scanner;
 
 public class Baekjoon2338 {
     public static void main(String[] args) {
-        Scanner sc = new Scanner(System.in);
-        BigDecimal A = new BigDecimal(sc.next());
-        BigDecimal B = new BigDecimal(sc.next());
-        sc.close();
+        try(Scanner sc = new Scanner(System.in)) {
+            BigDecimal A = new BigDecimal(sc.next());
+            BigDecimal B = new BigDecimal(sc.next());
 
-        System.out.println(A.add(B));
-        System.out.println(A.subtract(B));
-        System.out.println(A.multiply(B));
+            System.out.println(A.add(B));
+            System.out.println(A.subtract(B));
+            System.out.println(A.multiply(B));
+        }
+        catch(Exception e) {
+            System.out.println("숫자 써라.");
+        }
     }
 }
