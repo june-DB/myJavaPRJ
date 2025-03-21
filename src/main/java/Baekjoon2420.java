ac46609c948ef75b0add470ffbded2d0860fac66
도병준 <ihate3lue@gmail.com>
2025-03-21 18:15:14 +0900

    ADD issues

---
+++ src/main/java/Baekjoon2420.java
@@ -0,0 +1,24 @@
+import java.math.BigDecimal;
+import java.util.Scanner;
+
+public class Baekjoon2420 {
+    public static void main(String[] args) {
+        try(Scanner sc = new Scanner(System.in)) {
+            BigDecimal N = new BigDecimal(sc.next());
+            BigDecimal M = new BigDecimal(sc.next());
+
+            BigDecimal difference = N.subtract(M);
+
+            if (difference.compareTo(BigDecimal.ZERO) > 0) {
+                System.out.println(difference);
+            } else if (difference.compareTo(BigDecimal.ZERO) < 0) {
+                System.out.println(difference.negate());
+            } else {
+                System.out.println(0);
+            }
+        }
+        catch(Exception e) {
+            System.out.println("숫자 써라.");
+        }
+    }
+}
