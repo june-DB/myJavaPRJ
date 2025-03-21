9ba8d67ee421f1f583f518cd79cc57e792bcd5c6
도병준 <ihate3lue@gmail.com>
2025-03-21 11:01:09 +0900

    ADD issues

---
+++ src/main/java/Baekjoon2338.java
@@ -0,0 +1,14 @@
+import java.util.Scanner;
+
+public class Baekjoon2338 {
+    public static void main(String[] args) {
+        Scanner sc = new Scanner(System.in);
+        double A = sc.nextDouble();
+        double B = sc.nextDouble();
+        sc.close();
+
+        System.out.println(A + B);
+        System.out.println(A - B);
+        System.out.println(A * B);
+    }
+}
