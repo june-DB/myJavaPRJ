2f25d16bd7dd02ada5c5bcd6afec97ce64942fc3
도병준 <ihate3lue@gmail.com>
2025-03-21 16:07:45 +0900

    ADD issues

--- src/main/java/Baekjoon2338.java
+++ src/main/java/Baekjoon2338.java
@@ -1,14 +1,15 @@
+import java.math.BigDecimal;
 import java.util.Scanner;
 
 public class Baekjoon2338 {
     public static void main(String[] args) {
         Scanner sc = new Scanner(System.in);
-        double A = sc.nextDouble();
-        double B = sc.nextDouble();
+        BigDecimal A = new BigDecimal(sc.next());
+        BigDecimal B = new BigDecimal(sc.next());
         sc.close();
 
-        System.out.println(A + B);
-        System.out.println(A - B);
-        System.out.println(A * B);
+        System.out.println(A.add(B));
+        System.out.println(A.subtract(B));
+        System.out.println(A.multiply(B));
     }
 }
