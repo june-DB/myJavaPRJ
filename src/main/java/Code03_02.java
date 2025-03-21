8c1b891017cfeb6ecc3d0df979d251ab892c8699
도병준 <ihate3lue@gmail.com>
2025-03-21 16:08:17 +0900

    ADD issues

---
+++ src/main/java/Code03_02.java
@@ -0,0 +1,23 @@
+import java.util.Scanner;
+
+public class Code03_02 {
+    public static void main(String[] args) {
+        Scanner sc = new Scanner(System.in);
+        int n1, n2, res;
+        double res1;
+
+        n1 = sc.nextInt();
+        n2 = sc.nextInt();
+
+        res = n1 + n2;
+        System.out.println(res);
+        res = n1 - n2;
+        System.out.println(res);
+        res = n1 * n2;
+        System.out.println(res);
+        res1 = (double) n1 / n2;
+        System.out.println(res1);
+        res = n1 % n2;
+        System.out.println(res);
+    }
+}
