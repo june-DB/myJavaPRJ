ac5999fb4815164e37675ce76c6e941bdde6aa8f
도병준 <ihate3lue@gmail.com>
2025-03-21 15:09:31 +0900

    ADD issues

--- src/main/java/Code02_07.java
+++ src/main/java/Code02_07.java
@@ -2,18 +2,16 @@ public class Code02_07 {
     public static void main(String[] args) {
         int num1,  num2, result;
         num1 = 100;
-        num2 = 50;
+        num2 = 60;
 
-        result = num1 + num2;
-        System.out.println(num1 + " + " + num2 + " = " + result);
-
-        result = num1 - num2;
-        System.out.println(num1 + " - " + num2 + " = " + result);
-
-        result = num1 * num2;
-        System.out.println(num1 + "X" + num2 + "=" + result);
+        float res, res2;
 
         result = num1 / num2;
-        System.out.println(num1 + " / " + num2 + " = " + result);
+        res = num1 / num2;
+        res2 = (float) num1 / num2;
+
+        System.out.println(num1 + "/" + num2 + "=" + result);
+        System.out.println(num1 + "/" + num2 + "=" + res);
+        System.out.println(num1  + "/" + num2 + "=" + res2);
     }
 }
