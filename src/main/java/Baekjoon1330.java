dc2d57b894f87789be4bec882c782ea2b3cd9684
도병준 <ihate3lue@gmail.com>
2025-03-21 09:23:36 +0900

    ADD issues

--- src/main/java/Baekjoon1330.java
+++ src/main/java/Baekjoon1330.java
@@ -3,7 +3,7 @@ import java.util.Scanner;
 public class Baekjoon1330 {
     public static void main(String[] args) {
         Scanner sc = new Scanner(System.in);
-
+        System.out.println("숫자 2개 입력(띄어쓰기 구분) : ");
         int A = sc.nextInt();
         int B = sc.nextInt();
         sc.close();
