fd2b6b9efef602ca08b5a6327a975ac89b279354
도병준 <ihate3lue@gmail.com>
2025-03-25 17:29:21 +0900

    ADD issues

--- src/main/java/Practice2.java
+++ src/main/java/Practice2.java
@@ -1,6 +1,6 @@
 import java.util.Scanner;
 
-public class practice2 {
+public class Practice2 {
     public static void main(String[] args) {
         Scanner sc = new Scanner(System.in);
         int N = sc.nextInt();
