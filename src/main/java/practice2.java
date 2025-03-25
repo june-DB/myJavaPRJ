6b76b5a2893b6cdbe04d2b5b54bab178faa0c477
도병준 <ihate3lue@gmail.com>
2025-03-25 13:41:14 +0900

    ADD issues

---
+++ src/main/java/practice2.java
@@ -0,0 +1,21 @@
+import java.util.Scanner;
+
+public class practice2 {
+    public static void main(String[] args) {
+        Scanner sc = new Scanner(System.in);
+        int N = sc.nextInt();
+        sc.close();
+
+        if (N > 0 && N < 101) {
+            for (int i = 4; i <= N; i++) {
+                for (int j = 1; j <= i; j++) {
+                    System.out.print("*");
+                }
+                System.out.println();
+            }
+        }
+        else {
+            System.out.println("1이상 100 이하의 숫자 입력");
+        }
+    }
+}
