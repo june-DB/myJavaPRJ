9361cca6d2556fd5a5abbe29343c62be0fbe34e2
도병준 <ihate3lue@gmail.com>
2025-03-28 15:49:22 +0900

    ADD issues

---
+++ src/main/java/Code04_08.java
@@ -0,0 +1,6 @@
+public class Code04_08 {
+    public static void main(String[] args) {
+        String var1 = "난생처음 \n자바를 \n열공 중입니다.";
+        System.out.println(var1);
+    }
+}
---
+++ src/main/java/Code04_10.java
@@ -0,0 +1,11 @@
+public class Code04_10 {
+    public static void main(String[] args) {
+        String str = "난생 처음 자바";//공백도 하나의 문자다
+        int len;
+
+        len = str.length();
+
+        System.out.println("문자열 : " + str);
+        System.out.print("문자열 길이 : " + len);
+    }
+}
